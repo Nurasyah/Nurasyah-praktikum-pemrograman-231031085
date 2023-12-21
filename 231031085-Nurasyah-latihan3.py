edf2efe1c2d726536dcd81858d0a1d779151a25f
Nurasyah <153782886+Nurasyah@users.noreply.github.com>
2023-12-22 00:13:10 +0800

    uploud file latihan3

---
+++ 231031085-Nurasyah-latihan3.py
@@ -0,0 +1,13 @@
+# latihan 3
+
+nama = input("Nama Karyawan:")
+pendapatan = int(input("pendapatan:"))
+
+print("Nama Karyawan:", nama)
+print("pendapatan:", pendapatan)
+
+if pendapatan > 6000:
+    print("status: Berhak")
+
+else:
+    print("status: tidak berhak")
