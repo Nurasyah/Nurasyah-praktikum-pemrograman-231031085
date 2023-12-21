9fff5bb08ede21d27b6c6ded05a861a18bf45472
Nurasyah <153782886+Nurasyah@users.noreply.github.com>
2023-12-22 00:11:41 +0800

    uploud file latihan1

---
+++ 231031085-Nurasyah-latihan1.py
@@ -0,0 +1,14 @@
+# tugas 1
+# percabangan if
+nilai = 80
+batas_lulus = 65
+if nilai > batas_lulus:
+    print("Selamat, kamu lulus!")
+
+# percabangan else
+nilai = 50
+batas_lulus = 65
+if nilai > batas_lulus:
+    print("Selamat, kamu lulus!")
+else:
+    print("Sayang sekali, kamu tidak lulus!")
