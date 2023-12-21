050f9c3df6b5e05640631c562023137377c34b4e
Nurasyah <153782886+Nurasyah@users.noreply.github.com>
2023-12-21 08:42:06 +0800

    uploud file C5

---
+++ praktikum-c5.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("clear")
+
+a = True
+limit = 5
+i = 0
+
+while a:
+    i += 1
+    print(f'menjalankan program {limit + 1 - i}')
+    if i == limit:
+        a = False
+        print('program berhenti disini!')
+    else:
+        a = True
