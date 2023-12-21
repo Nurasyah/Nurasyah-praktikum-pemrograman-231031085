1a8aee064274bf00b9a8bd4cca252345ea3efbd7
Nurasyah <153782886+Nurasyah@users.noreply.github.com>
2023-12-22 00:33:50 +0800

    uploud file tugas-9-C

---
+++ 231031085-Nurasyah-tugas-9-C.py
@@ -0,0 +1,23 @@
+#tugas
+#buat variabel biodata
+
+biodata = { 'nama'          : 'Nurasyah',
+            'nim'           : 231031085,
+            'asal'          : 'pinrang',
+            'prodi'         : 'sistem informasi',
+            'tinggi'        : '152',
+            'tanggal lahir' : '13-12-2004',
+            'agama'         : 'islam',
+            'impian'        : 'lulus tepat wakktu',
+            'status'        : 'jomloh',
+            'hobi'          : { 'hb1'  : 'baca buku fiksi',
+                                'hb2'  : 'nonton movie',
+                                'hb3'  : 'rebahan'
+                              }
+            
+          }
+print(biodata)
+print(biodata['nama'])
+print (biodata['nim'])
+print(biodata['status'])
+print(biodata['hobi']['hb1'])
